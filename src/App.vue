3edc02376a63cf2389f657e694ea2dca66d7ef45
ConnorMac <cmacdougall78@gmail.com>
2018-03-25 01:38:12 +0200

    Removed all margins

--- src/App.vue
+++ src/App.vue
@@ -35,16 +35,18 @@ export default {
 </script>
 
 <style>
+html, body
+{
+    margin: 0px; padding: 0px
+}
 #app {
   font-family: 'Hind','Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 .container {
-  margin: 25px;
   display: grid;
   grid-template-columns: 2fr 8fr 2fr;
   grid-template-areas: 
