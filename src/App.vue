4396f963389f805150527cbb77579025e631d3e2
Gio <gio@incendiaryblue.com>
2018-03-25 00:57:31 +0200

    basic grid

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
-    <Home msg="Bequest"/>
-    <DashboardMenu />
-    <router-view></router-view>
+    <div class="container">
+      <Home msg="Bequest" class="header"/>
+      <DashboardMenu class="left-column"/>
+      <div class="content">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,5 +31,34 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  margin: 25px;
+  display: grid;
+  grid-template-columns: 2fr 8fr 2fr;
+  grid-template-areas: 
+  "header header header"
+  "leftColumn content content"
+  "footer footer footer";
+}
+.header {
+  grid-area: header;
+}
+.left-column {
+  grid-area: leftColumn;
+  display: grid;
+  grid-template-columns: 1fr;
+}
+.left-column a {
+    text-decoration: none;
+    color: #F57F17;
+    font-style: italic;
+    transition: all 0.3s;
+    /* Need this to prevent entire thing from being clickable! */
+    align-self: center;
+    justify-self: center;
+}
+.content {
+  grid-area: content;
 }
 </style>
