d0379959e90c121d9bd701e9d01b2f6d13242525
Gio <gio@incendiaryblue.com>
2018-03-25 00:43:06 +0200

    state management

--- src/main.js
+++ src/main.js
@@ -2,44 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import Vuex from 'vuex'
+import store from './store'
 import router from './router'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
-// // 1. Define route components.
-// // These can be imported from other files
-// const Foo = { template: '<div>foo</div>' }
-// const Bar = { template: '<div>bar</div>' }
-
-// // 2. Define some routes
-// // Each route should map to a component. The "component" can
-// // either be an actual component constructor created via
-// // `Vue.extend()`, or just a component options object.
-// // We'll talk about nested routes later.
-// const routes = [
-//   { path: '/', component: App },
-//   { path: '/foo', component: Foo },
-//   { path: '/bar', component: Bar }
-// ]
-
-// // 3. Create the router instance and pass the `routes` option
-// // You can pass in additional options here, but let's
-// // keep it simple for now.
-// const router = new VueRouter({
-//   routes // short for `routes: routes`
-// })
-
-// new Vue({
-//   render: h => h(App)
-// }).$mount('#app')
-
-// 4. Create and mount the root instance.
-// Make sure to inject the router with the router option to make the
-// whole app router-aware.
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
