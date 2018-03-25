ffd445411168004dec081a15e6187bdc144603ba
Gio <gio@incendiaryblue.com>
2018-03-25 08:14:18 +0200

    Added logout button

--- src/components/DashboardMenu.vue
+++ src/components/DashboardMenu.vue
@@ -4,5 +4,22 @@
       <router-link to="/tips" class="mui-btn mui-btn--flat">Tips</router-link>
       <router-link to="/embeds" class="mui-btn mui-btn--flat">Embeds</router-link>
       <router-link to="/withdraw" class="mui-btn mui-btn--flat">Withdraw</router-link>
+      <a class="mui-btn mui-btn--flat" v-on:click="logout()">Log out</a>
   </div>
 </template>
+
+<script>
+import store from "../store";
+
+export default {
+  name: "DashboardMenu",
+  methods: {
+    logout: function() {
+      localStorage.clear();
+      store.commit("changeLoggedIn", false);
+      store.commit("setUserDetails", { });
+      this.$router.push("/login");
+    }
+  }
+}
+</script>
