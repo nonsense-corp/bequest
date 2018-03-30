ca7ef32cabfdd81d0ab4b94dbf005c775fa37034
Gio <gio@incendiaryblue.com>
2018-03-30 21:44:11 +0200

    More styling

--- src/components/DashboardMenu.vue
+++ src/components/DashboardMenu.vue
@@ -19,13 +19,10 @@
       <router-link to="/tips" class="grid__sidebar-link">Tips</router-link>
       <router-link to="/embeds" class="grid__sidebar-link">Embeds</router-link>
       <router-link to="/withdraw" class="grid__sidebar-link">Withdraw</router-link>
-      <a v-on:click="logout()">Log out</a>
   </div>
 </template>
 
 <script>
-import store from "../store";
-
 export default {
   name: "DashboardMenu",
   data() {
@@ -33,13 +30,5 @@ export default {
       profileUrl: '/img/tumblr_na99e5A2bY1ti25hmo1_1280.60f478f5.jpg',
     }
   },
-  methods: {
-    logout: function() {
-      localStorage.clear();
-      store.commit("changeLoggedIn", false);
-      store.commit("setUserDetails", { });
-      this.$router.push("/login");
-    }
-  }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-      <h1 class="logo">Bequest</h1>
+    <a v-on:click="logout()" class="header_logout">LOGOUT ></a>
   </div>
 </template>
 
 <script>
+import store from "../store";
 export default {
   name: 'Header',
+  methods: {
+    logout: function() {
+      localStorage.clear();
+      store.commit("changeLoggedIn", false);
+      store.commit("setUserDetails", { });
+      this.$router.push("/login");
+    }
+  }
 }
 </script>
+
+<style scoped>
+  a {
+    color: #000;
+    letter-spacing: 0.1em;
+    font-size: 24px;
+    font-weight: 300;
+    cursor: pointer;
+  }
+</style>
